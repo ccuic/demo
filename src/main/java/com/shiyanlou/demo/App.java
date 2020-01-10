542a5c23894568b5225d2367674728cf0a84f1f7
ccuic <172704099@qq.com>
2020-01-10 21:26:45 +0800

    11111111

---
+++ src/main/java/com/shiyanlou/demo/App.java
@@ -0,0 +1,15 @@
+package com.shiyanlou.demo;
+
+import com.shiyanlou.demo.helloworld.HelloWorld;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	private static ApplicationContext context;
+
+	public static void main(String[] args) {
+		context=new ClassPathXmlApplicationContext("SpringBeans.xml");
+		HelloWorld obj=(HelloWorld)context.getBean("helloBean");
+		obj.printHello();
+	}
+}
